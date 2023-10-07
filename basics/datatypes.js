8e2a22a712bed21bf473f868a964e0cd0f53de7e
anamjavedd <63211018+anamjavedd@users.noreply.github.com>
2023-10-07 18:22:12 +0000

    Practice

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict" //treat all js code as newer version
+
+//alert(3+3) //we using nodejs and not browser
+
+let name = "anam" //string datatype
+
+let age = 18 //number datatype
+
+let isLoggedIn = false //boolean datatype
+
+// -----PRIMITIVE DATA TYPES-------
+//number => 2 to power 53
+//bigInt //used in trading websites
+//boolean
+// null => standalone value //value khaali h//empty//temperature abhi aya hi nhi h
+//undefined => value defined nhi hai
+//symbol => used in React alot -- used to tell that a component is unique
+
+
+//----------OBJECT-----------
+ 
---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let email = "javedanam1998@gmail.com"
+var accountPassword = "graco1234"
+accountCity = "Lucknow"
+
+//accountId = 23455 //not possible
+console.log(accountId);
+
+email = "javedanam70@gmail.com"
+accountPassword = "1234"
+accountCity = "Delhi" //should not be used
+
+console.table([email, accountPassword, accountCity, accountId])
