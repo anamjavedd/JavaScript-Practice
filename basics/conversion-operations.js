90d2f0d0cef3d5e0597896aa4a95859b5cda9e7b
anamjavedd <63211018+anamjavedd@users.noreply.github.com>
2023-10-07 18:35:59 +0000

    Conversion

---
+++ basics/conversion-operations.js
@@ -0,0 +1,16 @@
+let score = "33sdfghj"
+let score2 = null
+let decision = true
+let undefinedd = undefined
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+let score3 = Number(score2)
+let decision1 = Number(decision)
+let un = Number(undefinedd)
+
+console.log( valueInNumber);
+console.log(score3);
+console.log(decision1);
+console.log(un);  
