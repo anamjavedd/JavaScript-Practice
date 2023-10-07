47a759579f5b65ac50b40487259200401b0da4d0
anamjavedd <63211018+anamjavedd@users.noreply.github.com>
2023-10-07 18:52:46 +0000

    operations practice

--- basics/conversion-operations.js
+++ basics/conversion-operations.js
@@ -14,3 +14,44 @@ console.log( valueInNumber);
 console.log(score3);
 console.log(decision1);
 console.log(un);  
+
+
+// ************OPERATIONS**********
+
+let value = 2
+
+let negvalue = -value
+
+console.log(negvalue);
+
+let str1 = "anam"
+let str2 = " hello"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+console.log("1" + 2+2);
+
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+// AVOID THESE CONVERSIONS
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+ //In js == and <, > are treated differently. In the latter null is treated as 0
+console.log(undefined == 0);
+
+console.log("2" === 2);
+ 
+
+
+
+
+
